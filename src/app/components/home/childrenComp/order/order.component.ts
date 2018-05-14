1e61afaffbb016267a75021699a927f47096b0ab
Sarvesh <sarveshgorwadkar@gmail.com>
2018-05-14 09:00:26 +0530

    Order Push Complete

--- src/app/components/home/childrenComp/order/order.component.ts
+++ src/app/components/home/childrenComp/order/order.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { MenuItemService } from '../../../../services/childServices/menu-item.service';
+import { OrderService } from '../../../../services/childServices/order-service.service';
+import { ToastsManager } from 'ng2-toastr';
 
 @Component({
   selector: 'app-order',
@@ -20,50 +22,32 @@ export class OrderComponent implements OnInit {
 
   tabActive:string = 'tableorder';
 
-  //All Orders Array.
-  allOrdersArray:any = [];
-
   //tableSpecific Orders
   activeTable: any = 1;
   allTableOrders:any = [];
+  activetableOrder:any;
+
   //For Online Orders
   activeOrder = 0;
   detailOrder:any;
-  allOnlineOrders:any = [{
-    "orderNumber":1,
-    "orderSource":"zomato",
-    "orderName":"abc",
-    "totalBill":80,
-    "orderItems":[{
-        "itemName":"pohe",
-        "qty":2,
-        "price": 20
-    },{
-        "itemName":"upma",
-        "qty":2,
-        "price": 20
-    }]
-  }];
+  allOnlineOrders:any = [];
 
-  //For Parvel Orders
+  //all Parcel Orders
+
   parcelOrders:number = 10;
   allParcelOrders:any = [];
 
-  constructor(private menuService:MenuItemService) { }
-
+  constructor(private menuService:MenuItemService ,
+              private orderService : OrderService,
+              public toastr: ToastsManager) { }
   ngOnInit() {
 
-    this.allOrdersArray['tableOrders'] = this.allTableOrders;
-    this.allOrdersArray['parcelOrders'] = this.allParcelOrders;
-    this.allOrdersArray['onlinOrders'] = this.allOnlineOrders;
-
-    if(sessionStorage.getItem('allOrders')){
-      this.allOrdersArray = sessionStorage.getItem('allOrders');
-    }else{
-      sessionStorage.setItem('allOrders' , this.allOrdersArray);
-    }
-
     this.totaltables = parseInt(sessionStorage.getItem('tableCount'));
+
+    this.allOnlineOrders = this.orderService.getOnlineOrdersObject();
+    this.allTableOrders = this.orderService.getTableOrdersObject();
+    this.allParcelOrders = this.orderService.getParcelOrdersObject()
+
     this.detailOrder = this.allOnlineOrders[0];
 
     this.menuService.getAll().subscribe((result) => {
@@ -75,44 +59,65 @@ export class OrderComponent implements OnInit {
     });
 
     this.assignTableData();
-
   }
-
 
   groupBy(xs, f) {
     return xs.reduce((r, v, i, a, k = f(v)) => ((r[k] || (r[k] = [])).push(v), r), {});
   }
 
+  selectedTable(tableNumber){
+    this.activeTable = tableNumber + 1;
+    this.activetableOrder = this.allTableOrders[tableNumber];
+  }
 
   addItemToOrder(item){
 
+    let orderedItem:any = {};
+    let dateObj = new Date();
+    orderedItem['name'] = item['name'];
+    orderedItem['orderedqty'] = item.forderdQty ? item.forderdQty : item.orderdQty;
+    orderedItem['totalprice'] = (orderedItem['orderedqty'] * item['sellingprice']);
+    orderedItem['totalcost'] = (orderedItem['orderedqty'] * item['costprice']);
+    orderedItem['sellingprice'] = item['sellingprice']
+    orderedItem['category'] = item['category'];
 
-    let orderedItem:any;
+    this.activetableOrder.totalBill = this.activetableOrder.totalBill + orderedItem['totalprice'];
 
-    console.log(item);
+    this.activetableOrder.orderedItems.push(orderedItem);
 
-    item.forderdQty = '';
+    if(this.activetableOrder.orderedItems.length == 1){
+      this.activetableOrder['startTime'] = dateObj.getTime();
+    }else if(this.activetableOrder.orderedItems.length == 0){
+      this.activetableOrder['startTime'] = 0;
+    }
+
+    if(this.activetableOrder.orderedItems.length > 0){
+      this.activetableOrder.status = 'busy';
+    }
+
     item.orderdQty = '';
-
+    item.forderdQty = '';
   }
 
-  counter(i: number) {
-    return new Array(i);
+  qtyChanged(index , item){
+    let newPrice = (item['orderedqty'] * item['sellingprice']);
+    this.activetableOrder.totalBill = this.activetableOrder.totalBill - item['totalprice'] + newPrice;
+    this.activetableOrder.orderedItems[index]['totalprice'] = newPrice;
   }
-  selectedTable(tableNumber){
-    this.activeTable = tableNumber + 1;
 
-
-
-
-
+  removeOrderedItem(index , item){
+    this.activetableOrder.totalBill = this.activetableOrder.totalBill - item['totalprice'];
+    this.activetableOrder.orderedItems.splice(index , 1);
+    if(this.activetableOrder.orderedItems.length == 0){
+      this.activetableOrder.status = 'open';
+    }
   }
 
   selectCategory(clickedCategory){
       this.selectedCategory = clickedCategory;
   }
-  scroll(ele , direction){
 
+  scroll(ele , direction){
       let maxWidth = ele._scrollLeftMax;
       if(direction == 'left' && this.Scrolled >= 200){
           this.Scrolled = this.Scrolled - 300;
@@ -123,18 +128,7 @@ export class OrderComponent implements OnInit {
       }
   }
 
-
-
-
-
-
-
-
-
-
-
   //Online order functions...
-
   showOrderDetails(onlineOrder){
       this.activeOrder = onlineOrder;
       this.detailOrder = this.allOnlineOrders[onlineOrder];
@@ -145,46 +139,53 @@ export class OrderComponent implements OnInit {
       this.activeOrder = 0;
       this.detailOrder = this.allOnlineOrders[0];
       this.submitOrder(detailOrder);
-
   }
-
-
   // Common submit order function .
   submitOrder(order:any){
 
+  this.allTableOrders[order.tableNumber] = this.getNewTableObject(order.tableNumber);
+  this.activetableOrder = this.allTableOrders[order.tableNumber];
 
+  let orderObject:any = {};
+  let dateObj = new Date();
 
+  orderObject['customername'] = "abc";
+  orderObject['year'] = dateObj.getFullYear();
+  orderObject['month'] = dateObj.getMonth() + 1;
+  orderObject['day'] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'][dateObj.getDay()];
+  orderObject['date'] = dateObj.getDate();
+  orderObject['hour'] =dateObj.getHours();
+  orderObject['totaltime'] = dateObj.getTime() - order['startTime'];
+  orderObject['totalbillamt'] = order['totalBill'];
+  orderObject['waitingtime'] = 0;
+  orderObject['ordermethod'] = this.tabActive;
+  orderObject['orderedItems'] = order.orderedItems;
+  orderObject['paymentmethod'] = order.paymentmethod;
 
-
-
-
+    this.orderService.post(orderObject).subscribe((result) => {
+      this.toastr.success('Order Successfully Submitted' ,"Success" , {showCloseButton : true});
+    });
 
   }
-
-
   //AssignTable Objects to tableOrder Functions
   assignTableData(){
-
-
     for(let i = 0; i < this.totaltables ; i++){
-        let tableInfoObj:any = {};
-
-        tableInfoObj['tableNumber'] = i;
-        tableInfoObj['totalBill'] = 0;
-        tableInfoObj['orderedItems'] = [];
-        tableInfoObj['status'] = '';
-        tableInfoObj['startTime'] = '';
-        this.allTableOrders.push(tableInfoObj);
+        this.allTableOrders.push(this.getNewTableObject(i));
     }
-
-    console.log(this.allTableOrders , this.allOrdersArray)
-
+    this.activetableOrder = this.allTableOrders[0];
   }
 
+  getNewTableObject(index){
+    let tableInfoObj:any = {};
 
+    tableInfoObj['tableNumber'] = index;
+    tableInfoObj['totalBill'] = 0;
+    tableInfoObj['orderedItems'] = [];
+    tableInfoObj['status'] = 'open';
+    tableInfoObj['startTime'] = '';
+    tableInfoObj['paymentmethod'] = 'cash';
 
-
-
-
+    return tableInfoObj;
+  }
 
 }
--- src/app/services/childServices/order-service.service.ts
+++ src/app/services/childServices/order-service.service.ts
@@ -1,12 +1,47 @@
 import { Injectable } from '@angular/core';
+import { DataService } from '../data.service';
+import { Http } from '@angular/http';
+import { environment } from '../../../environments/environment';
 
 @Injectable()
-export class OrderService {
+export class OrderService extends DataService{
 
-  constructor() { }
 
-  
+  allParcelOrders:any = [];
 
+  allOnlineOrders:any = [{
+    "orderNumber":1,
+    "orderSource":"zomato",
+    "orderName":"abc",
+    "totalBill":80,
+    "orderItems":[{
+        "itemName":"pohe",
+        "qty":2,
+        "price": 20
+    },{
+        "itemName":"upma",
+        "qty":2,
+        "price": 20
+    }]
+  }];
+
+  allTableOrders:any = [];
+
+  constructor(http: Http) {
+    super((environment.dataURL + "saveOrder"), http);
+  }
+
+  getTableOrdersObject(){
+    return this.allTableOrders;
+  }
+
+  getParcelOrdersObject(){
+    return this.allParcelOrders;
+  }
+
+  getOnlineOrdersObject(){
+    return this.allOnlineOrders;
+  }
 
 
 }
